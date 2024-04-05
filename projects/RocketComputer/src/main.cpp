a75d2d9f363821db2ee8938fefb7148c879e0e38
Krzysiek <krzysztof.radawiec020@gmail.com>
2024-04-05 20:18:20 +0200

    relay

--- projects/RocketComputer/src/main.cpp
+++ projects/RocketComputer/src/main.cpp
@@ -1,25 +1,25 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// Definicja pinu diody LED
-const int buzzPin = 25;
-const int potPin =32 ;
-int val ;
-void setup() {
-  // Ustawienie pinu diody LED jako wyjście
-  pinMode(buzzPin, OUTPUT);
-  pinMode(potPin , INPUT);
-  Serial.begin(9600);
-}
+// // Definicja pinu diody LED
+// const int buzzPin = 25;
+// const int potPin =32 ;
+// int val ;
+// void setup() {
+//   // Ustawienie pinu diody LED jako wyjście
+//   pinMode(buzzPin, OUTPUT);
+//   pinMode(potPin , INPUT);
+//   Serial.begin(9600);
+// }
 
-void loop() {
+// void loop() {
   
 
   
-  val=analogRead(potPin);
-  Serial.println(val);
+//   val=analogRead(potPin);
+//   Serial.println(val);
   
 
-  // Włączenie diody LED
-  analogWrite(buzzPin, val/16);
- delay(1000);
-}
+//   // Włączenie diody LED
+//   analogWrite(buzzPin, val/16);
+//  delay(1000);
+// }
