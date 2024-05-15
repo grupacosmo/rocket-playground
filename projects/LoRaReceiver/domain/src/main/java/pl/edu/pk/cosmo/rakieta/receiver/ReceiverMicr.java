85ac778146b8e84cd5150e5cfea8d8cd2464ad87
Aleksy Biela <aleks.302@wp.pl>
2024-05-16 00:08:00 +0200

    delete useless files

--- projects/LoRaReceiver/domain/src/main/java/pl/edu/pk/cosmo/rakieta/receiver/ReceiverMicr.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pk.cosmo.rakieta.receiver;
-
-import java.io.DataInputStream;
-import java.io.IOException;
-
-public interface ReceiverMicr{
-    public void getPort(int BaudRate) throws IOException;
-    public void readAsData(DataInputStream dis);
-}
