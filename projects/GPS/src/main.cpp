4655ed566e801f09f5e355e35fcf12b4447eb7e3
Szymon Chwastek <chwastekszymon@gmail.com>
2024-05-22 20:26:27 +0200

    feat: data fetch and display

--- projects/GPS/src/main.cpp
+++ projects/GPS/src/main.cpp
@@ -1,32 +1,33 @@
 #include <TinyGPSPlus.h>
+#include <HardwareSerial.h>
 
-// Sample NMEA - to be changed
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+#define GPS_TX 16 
+#define GPS_RX 17
+
+HardwareSerial mySerial(1);
 
 TinyGPSPlus gps;
 
+String fetchLocation();
+
 void displayInfo();
 
-void setup()
-{
-  Serial.begin(9600);
-
-  while (*gpsStream)
-    if (gps.encode(*gpsStream++))
-      displayInfo();
-
-  Serial.println();
-  Serial.println(F("Done."));
+void setup() {
+  Serial.begin(115200); 
+  mySerial.begin(9600, SERIAL_8N1, GPS_TX, GPS_RX); 
+  Serial.println("GPS Serial initialized");
 }
 
-void loop()
-{
+void loop() {
+  String incomingData = fetchLocation();
+  Serial.println(incomingData);
+  const char* dataPtr = incomingData.c_str();
+  while (*dataPtr) {
+    if (gps.encode(*dataPtr++)) {
+      displayInfo();
+    }
+  }
+  delay(1000);
 }
 
 void displayInfo()
@@ -71,6 +72,7 @@ void displayInfo()
     Serial.print(F("."));
     if (gps.time.centisecond() < 10) Serial.print(F("0"));
     Serial.print(gps.time.centisecond());
+    Serial.print(F("UTC"));
   }
   else
   {
@@ -78,4 +80,13 @@ void displayInfo()
   }
 
   Serial.println();
+}
+
+String fetchLocation(){
+  String incomingData = "";
+  while (mySerial.available()) {
+    char incomingByte = mySerial.read();
+    incomingData += incomingByte;
+  }
+  return incomingData;
 }
